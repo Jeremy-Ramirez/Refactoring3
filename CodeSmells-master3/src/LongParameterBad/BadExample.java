11b4ead2e8dbbbfbd36a4dd8410c0ece34589c25
diacrami <diacrami@espol.edu.ec>
2020-08-28 00:04:06 -0500

    Correccion

--- CodeSmells-master3/src/LongParameterBad/BadExample.java
+++ CodeSmells-master3/src/LongParameterBad/BadExample.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package LongParameterBad;
 import java.util.Random;
-/**
- *
- * @author gabpa
- */
+
+
 public class BadExample {
 	
 	
