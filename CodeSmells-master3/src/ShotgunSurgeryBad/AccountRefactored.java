a599394a542bc87b81f996b23bbd5bd261ea8fbd
Ronny-Andrade <66749405+Ronny-Andrade@users.noreply.github.com>
2020-08-30 00:41:37 -0500

    ShotGunSurgery

--- CodeSmells-master3/src/ShotgunSurgeryBad/AccountRefactored.java
+++ CodeSmells-master3/src/ShotgunSurgeryBad/AccountRefactored.java
@@ -1,53 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ShotgunSurgeryBad;
 
-/**
- *
- * @author https://dzone.com/articles/code-smell-shot-surgery
- */
-public class Account {
+public class AccountRefactored {
 
        private String type;
        private String accountNumber;
        private int amount;
 
-       public Account(String type,String accountNumber,int amount)
+       public AccountRefactored(String type,String accountNumber,int amount)
        {
               this.amount=amount;
               this.type=type;
               this.accountNumber=accountNumber;
        }
 
+       private boolean isAccountUnderflow()
+       {
+             return amount<=500;
+       }
+
        public void debit(int debit) throws Exception
        {
-              if(amount <= 500)
+              if(isAccountUnderflow())
               {
                      throw new Exception("Mininum balance shuold be over 500");
               }
-			  
               amount = amount-debit;
               System.out.println("Now amount is" + amount);
        }
 
-       public void transfer(Account from,Account to,int cerditAmount) throws Exception
+       public void transfer(AccountRefactored from,AccountRefactored to,int creditAmount) throws Exception
        {
-              if(from.amount <= 500)
+              if(isAccountUnderflow())
               {
                      throw new Exception("Mininum balance shuold be over 500");
               }
-              to.amount = amount+cerditAmount;
+              to.amount = amount+creditAmount;
        }
 
        public void sendWarningMessage()
        {
-              if(amount <= 500)
+              if(isAccountUnderflow())
               {
                      System.out.println("amount should be over 500");
               }
        }
-	   
+
 }
