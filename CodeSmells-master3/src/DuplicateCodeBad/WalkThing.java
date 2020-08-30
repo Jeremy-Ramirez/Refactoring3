dab45102602d9a35a34ec90bcbd20b5372f654c8
Ronny-Andrade <66749405+Ronny-Andrade@users.noreply.github.com>
2020-08-30 00:14:36 -0500

    DuplicateCode-Consolidate

--- CodeSmells-master3/src/DuplicateCodeBad/WalkThing.java
+++ CodeSmells-master3/src/DuplicateCodeBad/WalkThing.java
@@ -1,47 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DuplicateCodeBad;
 
-/**
- *
- * @author gabpa
- */
 public class WalkThing {
+	private Player Cplayer;
 	
-	private Player player;
-	
-	public Position WalkNorth()
+	public Position Walk(String direction)
 	{
 	   Player player = GetPlayer();
-	   player.Move("N");
+	   player.Move(direction);
 	   return player.NewPosition;
-	}
-
-	public Position WalkSouth()
-	{
-	   Player player = GetPlayer();
-	   player.Move("S");
-	   return player.NewPosition;
-	}
-
-	public Position WalkEast()
-	{
-	   Player player = GetPlayer();
-	   player.Move("E");
-	   return player.NewPosition;
-	}
-
-	public Position WalkWest()
-	{
-	   Player player = GetPlayer();
-	   player.Move("W");
-	   return player.NewPosition;
-	}
+	} 
 	
 	public Player GetPlayer(){
-		return this.player;
+		return this.Cplayer;
 	}
 }
